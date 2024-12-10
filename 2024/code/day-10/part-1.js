d4fa8ddd2ac8647c301ddcbb544b895e0248dd55
Justin Rasink <justin@mac.local>
2024-12-10 14:37:36 +0100

    2024 day 10

---
+++ 2024/code/day-10/part-1.js
@@ -0,0 +1,54 @@
+export default (input) => {
+  const map = input.split('\n').map((s) => s.split('').map((s) => parseInt(s, 10)));
+  const height = map.length;
+  const width = map[0].length;
+
+  const inside = ({ y, x }) => y >= 0 && y < height && x >= 0 && x < width;
+
+  const neighbours = ({ x, y }) => [
+    { x: x - 1, y },
+    { x: x + 1, y },
+    { x, y: y - 1 },
+    { x, y: y + 1 }
+  ].filter(inside);
+
+  const next = (p) => neighbours(p).filter((n) => (map[n.y][n.x] - map[p.y][p.x]) === 1);
+
+  const unique = (ps) => {
+    const rs = [];
+    const m = Array(height * width);
+    for (const p of ps) {
+      const { x, y } = p;
+      const i = width * y + x;
+      if (!m[i]) {
+        rs.push({ x, y });
+        m[i] = true;
+      }
+    }
+    return rs;
+  };
+
+  const hs = [];
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (map[y][x] === 0) {
+        hs.push({ x, y });
+      }
+    }
+  }
+
+  const scan = (start) => {
+    let ps = [start];
+
+    for (let k = 0; k < 9; k++) {
+      ps = unique([].concat(...ps.map(next)));
+    }
+
+    return ps.length;
+  }
+
+  const r = hs.map(scan).reduce((r, n) => r + n, 0);
+
+  return r;
+}
---
+++ 2024/code/day-10/part-2.js
@@ -0,0 +1,40 @@
+export default (input) => {
+  const map = input.split('\n').map((s) => s.split('').map((s) => parseInt(s, 10)));
+  const height = map.length;
+  const width = map[0].length;
+
+  const inside = ({ y, x }) => y >= 0 && y < height && x >= 0 && x < width;
+
+  const neighbours = ({ x, y }) => [
+    { x: x - 1, y },
+    { x: x + 1, y },
+    { x, y: y - 1 },
+    { x, y: y + 1 }
+  ].filter(inside);
+
+  const next = (p) => neighbours(p).filter((n) => (map[n.y][n.x] - map[p.y][p.x]) === 1);
+
+  const hs = [];
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      if (map[y][x] === 0) {
+        hs.push({ x, y });
+      }
+    }
+  }
+
+  const scan = (start) => {
+    let ps = [start];
+
+    for (let k = 0; k < 9; k++) {
+      ps = [].concat(...ps.map(next));
+    }
+
+    return ps.length;
+  }
+
+  const r = hs.map(scan).reduce((r, n) => r + n, 0);
+
+  return r;
+}
